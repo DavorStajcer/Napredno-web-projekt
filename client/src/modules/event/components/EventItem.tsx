201a3b33f1f825c233f09dc9bc7b3bfcdfe9d24e
markoboras <mboras1@etfos.hr>
2022-01-09 22:37:38 +0100

    name in navbar

--- client/src/modules/event/components/EventItem.tsx
+++ client/src/modules/event/components/EventItem.tsx
@@ -25,7 +25,6 @@ export const EventItem: React.FC = () => {
           <Card
             sx={{
               height: '100%',
-              width: '100%',
               display: 'flex',
               flexDirection: 'column',
             }}
